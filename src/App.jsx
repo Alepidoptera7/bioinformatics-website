75c1bb5350009f8223a31f4eb73f85633b40158b
unknown <quin_lamothe@berkeley.edu>
2025-05-18 16:35:00 -0700

    Fix Router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import DataAnalytics from './pages/dataAnalytics';
 import DifferentialExpression from './pages/differentialExpression';
 import MachineLearning from './pages/machineLearning';
 import WebDevelopment from './pages/webDevelopment';
-import BioinformaticApis from './pages/BioinformaticApis';
+import BioinformaticApis from './pages/bioinformaticApis'; // lowercase "A" to match filename
 
 function Home() {
   return (
@@ -24,33 +24,35 @@ function Home() {
           This is my personal site, which is designed to showcase some of my work and tools I've developed.
         </p>
       </section>
+
       <section>
-  <h2>Education:</h2>
-  <ul>
-    <li>
-      UC Santa Cruz,{" "}
-      <a href="https://bme.ucsc.edu/" target="_blank" rel="noopener noreferrer">
-        Biomolecular Engineering and Bioinformatics BS '22
-      </a>
-    </li>
-    <li>
-      UC Santa Cruz,{" "}
-      <a href="https://www.math.ucsc.edu/" target="_blank" rel="noopener noreferrer">
-        Mathematics BA '22
-      </a>
-    </li>
-    <li>
-      UC Berkeley,{" "}
-      <a href="https://bioeng.berkeley.edu/meng" target="_blank" rel="noopener noreferrer">
-        Bioengineering – Bioinformatics MEng '25
-      </a>
-    </li>
-  </ul>
-</section>
+        <h2>Education:</h2>
+        <ul>
+          <li>
+            UC Santa Cruz,&nbsp;
+            <a href="https://bme.ucsc.edu/" target="_blank" rel="noopener noreferrer">
+              Biomolecular Engineering and Bioinformatics BS '22
+            </a>
+          </li>
+          <li>
+            UC Santa Cruz,&nbsp;
+            <a href="https://www.math.ucsc.edu/" target="_blank" rel="noopener noreferrer">
+              Mathematics BA '22
+            </a>
+          </li>
+          <li>
+            UC Berkeley,&nbsp;
+            <a href="https://bioeng.berkeley.edu/meng" target="_blank" rel="noopener noreferrer">
+              Bioengineering – Bioinformatics MEng '25
+            </a>
+          </li>
+        </ul>
+      </section>
+
       <section>
         <h2>Technical Skills:</h2>
         <ul>
-          <li><Link to="/data-analytics">Bioinformatic Data Analytic</Link></li>
+          <li><Link to="/data-analytics">Bioinformatic Data Analytics</Link></li>
           <li><Link to="/differential-expression">RNA-seq Differential Expression</Link></li>
           <li><Link to="/machine-learning">Machine Learning</Link></li>
           <li><Link to="/web-development">Web Development and Server Design</Link></li>
@@ -67,14 +69,14 @@ function Home() {
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/bioinformatics-website">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/data-analytics" element={<DataAnalytics />} />
         <Route path="/differential-expression" element={<DifferentialExpression />} />
         <Route path="/machine-learning" element={<MachineLearning />} />
         <Route path="/web-development" element={<WebDevelopment />} />
-        <Route path="/bioinformatic-Apis" element={<BioinformaticApis />} />
+        <Route path="/bioinformatic-apis" element={<BioinformaticApis />} />
       </Routes>
     </Router>
   );
